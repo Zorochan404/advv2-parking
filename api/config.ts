40a1737bc85ce575b0a2f52f58bce302c7232709
Ritav Kashyap <ritavkashyap123@gmail.com>
2026-02-02 21:16:44 +0530

    endpoint fixed

--- api/config.ts
+++ api/config.ts
@@ -1,7 +1,7 @@
 import { useAuthStore } from "@/store/authStore";
 
 export const API_CONFIG = {
-    BASE_URL: process.env.EXPO_PUBLIC_API_URL || 'https://7e720107c12a.ngrok-free.app/api/v1',
+    BASE_URL: process.env.EXPO_PUBLIC_API_URL || 'https://api.57.159.30.237.nip.io/api/v1',
     TIMEOUT: 10000,
     HEADERS: {
         'Content-Type': 'application/json',
